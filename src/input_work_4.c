8d1f78e6b8d244ef37a352e3984e21857bcec2e5
Tamas Tokesi <ttokesi@3-F-11.42heilbronn.de>
2022-02-01 19:25:54 +0100

    finished working looping expander

--- src/input_work_4.c
+++ src/input_work_4.c
@@ -8,22 +8,17 @@ int	size_dollar_point(char **str, t_data *data)
 	len = 0;
 	if ((**str == '$' && data->qusingle == 0))
 	{
-		printf("%d\n", ft_isalnum(**str));
-			(*str)++;
 			len++;
-		printf("%c\n", **str);
-		printf("%d\n", ft_isalnum(**str));
+			(*str)++;
 		while (ft_isalnum(**str) == 1 && **str != '\0')
 		{
-		printf("%d\n", ft_isalnum(**str));
 			len++;
 			(*str)++;
 		}
 	}
 	else
 	{
-		printf("fuck\n");
-		while (**str != '$' && **str != '\0')
+		while ((**str != '$' && **str != '\0' )|| data->qusingle == 1)
 		{
 			check_token_flags_li(**str, data);
 			len++;
@@ -44,7 +39,6 @@ char *next_part(char **str, t_data *data)
 	if (*temp == '$'&& data->qusingle == 0)
 	{
 		buf = expand_clean_dollar(ft_substr(temp, 0, len), data);
-		printf("fuck: %s\n", ft_substr(temp, 0, len));
 	}
 	else
 		buf = ft_substr(temp, 0, len);
@@ -67,13 +61,17 @@ char	*expand_next_part(char *str, t_data *data)
 	while (*str != '\0')
 	{
 		// check_token_flags_li(*str, data);
-		// if (buf[0])
-		// {
-		// 	superholder = save_and_staff(&buf, &superholder);
-		// 	if (check_end(&buf))
-		// 		return (superholder);
-		// }
 		buf = next_part(&str, data);
+		if (buf[0])
+		{
+			// superholder = save_and_staff(&buf, &superholder);
+			if (superholder == NULL)
+				superholder = ft_strdup(buf);
+			else
+				superholder = ft_strjoin(superholder, buf);
+			// if (check_end(&buf))
+			// 	return (superholder);
+		}
 
 
 
@@ -81,6 +79,6 @@ char	*expand_next_part(char *str, t_data *data)
 		// 	return (0);
 		// str++;
 	}
-	// return (superholder);
-	return (buf);
+	return (superholder);
+	// return (buf);
 }
