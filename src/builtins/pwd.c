5a9846487369eba07887269034ee3eaf2be5fe86
Sonja Reinholz <sreinhol@3-F-10.42heilbronn.de>
2022-03-05 00:29:58 +0100

    fixed pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -14,26 +14,12 @@
 
 void	builtin_pwd(t_data *data)
 {
-	char	**temp_env;
-	char	*path_pwd;
-	char	**pwd;
-	int		i;
+	char	*pwd;
 
-	*temp_env = *data->environ;
-	pwd = NULL;
-	while (*temp_env != NULL && ft_strncmp(*temp_env, "PWD=", 4))
-		(*temp_env)++;
-	path_pwd = (*temp_env + 4);
-	pwd = ft_split(path_pwd, ':');
+	pwd = getcwd(NULL, 0);
 	if (!pwd)
 		msg_exit(data, "malloc error");
-	i = 0;
-	while (pwd[i] != NULL)
-	{
-		printf("%s\n", pwd[i]);
-		i++;
-	}
-	ft_free_array(pwd);
-	free((void *) pwd[i]);
+	printf("%s\n", pwd);
+	free(pwd);
 	g_exit = 0;
 }
